f41454257ecf42a04ec454d876cb832ccf5e7817
saschagobel <goebel@hertie-school.org>
2025-12-02 16:48:56 +0100

    feat: add core data processing utilities

---
+++ src/tlmtc/config.py
@@ -0,0 +1,16 @@
+"""
+Transfer Learning for Multi-Label Text Classification.
+
+Default configuration
+"""
+
+
+from pathlib import Path
+
+# PATHS
+BASE_PATH = Path(__file__).parent
+DATA_PATH = BASE_PATH.parent / 'data'
+RAW_DATA_PATH = DATA_PATH / 'raw.csv'
+TRAIN_DATA_PATH = DATA_PATH / 'train.parquet'
+VAL_DATA_PATH = DATA_PATH / 'val.parquet'
+TEST_DATA_PATH = DATA_PATH / 'test.parquet'
