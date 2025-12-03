cb5904f4648da4cd5632d0c5b5c03bb05e678af0
saschagobel <goebel@hertie-school.org>
2025-12-03 17:15:41 +0100

    fix: double quotes

--- src/tlmtc/config.py
+++ src/tlmtc/config.py
@@ -9,8 +9,8 @@ from pathlib import Path
 
 # PATHS
 BASE_PATH = Path(__file__).parent
-DATA_PATH = BASE_PATH.parent / 'data'
-RAW_DATA_PATH = DATA_PATH / 'raw.csv'
-TRAIN_DATA_PATH = DATA_PATH / 'train.parquet'
-VAL_DATA_PATH = DATA_PATH / 'val.parquet'
-TEST_DATA_PATH = DATA_PATH / 'test.parquet'
+DATA_PATH = BASE_PATH.parent / "data"
+RAW_DATA_PATH = DATA_PATH / "raw.csv"
+TRAIN_DATA_PATH = DATA_PATH / "train.parquet"
+VAL_DATA_PATH = DATA_PATH / "val.parquet"
+TEST_DATA_PATH = DATA_PATH / "test.parquet"
