e68fa61da6e2130c22c8dee6eaf934b1f3d97b21
saschagobel <goebel@hertie-school.org>
2025-12-04 15:04:31 +0100

    chore: update config

--- src/tlmtc/config.py
+++ src/tlmtc/config.py
@@ -10,6 +10,21 @@ from pathlib import Path
 BASE_PATH = Path(__file__).parent
 DATA_PATH = BASE_PATH.parent / "data"
 RAW_DATA_PATH = DATA_PATH / "raw.csv"
+RAW_TEST_DATA_PATH = DATA_PATH / "raw_test.csv"
 TRAIN_DATA_PATH = DATA_PATH / "train.parquet"
 VAL_DATA_PATH = DATA_PATH / "val.parquet"
 TEST_DATA_PATH = DATA_PATH / "test.parquet"
+
+# MODEL CHECKPOINTS
+CHECKPOINT = "microsoft/deberta-v3-base"
+
+# SPLITTING
+VALIDATION_SIZE = 0.15
+TEST_SIZE = 0.15
+RANDOM_SEED = 2469
+
+# WORKFLOW
+HYPERPARAMETER_TUNING = True
+
+# HYPERPARAMETERS
+SEQUENCE_LENGTH = 128
