92f3a153d277979015cd8a0bf61783ba06994cf2
saschagobel <sascha.gbl@googlemail.com>
2026-04-29 21:14:55 +0200

    feat: add label metrics reporting table

--- src/tlmtc/reporting.py
+++ src/tlmtc/reporting.py
@@ -71,3 +71,57 @@ def make_global_metrics_table(
         )
         .tab_options(stub_border_style="none")
     )
+
+
+def make_label_metrics_table(
+    eval_metrics: dict[str, dict[str, float]],
+    target_name: str,
+    checkpoint: str,
+    train_data_path: Path,
+    test_data_path: Path,
+) -> GT:
+    """Create a formatted summary table of label-specific evaluation metrics."""
+    df = pd.DataFrame(eval_metrics).T.reset_index()
+    df = df.rename(
+        columns={
+            "index": "Label",
+            "f1": "F1",
+            "precision": "Precision",
+            "recall": "Recall",
+            "roc_auc": "ROC-AUC",
+            "pr_auc": "PR-AUC",
+        }
+    )
+
+    model_name = checkpoint.rsplit("/", maxsplit=1)[-1]
+    train_examples = len(pd.read_parquet(train_data_path))
+    test_examples = len(pd.read_parquet(test_data_path))
+
+    return (
+        GT(df)
+        .tab_header(
+            title=md(f"**Multi-label Classification of {target_name}**"),
+            subtitle=md(f"*Performance metrics for fine-tuned {model_name}*"),
+        )
+        .cols_label(
+            true_support=md("True<br>support"),
+            pred_support=md("Pred<br>support"),
+        )
+        .fmt_number(
+            columns=[
+                "F1",
+                "Precision",
+                "Recall",
+                "ROC-AUC",
+                "PR-AUC",
+                "true_support",
+                "pred_support",
+            ],
+            decimals=2,
+        )
+        .tab_source_note(source_note=md(f"*Train examples*: {train_examples}<br>*Test examples*: {test_examples}"))
+        .tab_style(
+            style=style.text(weight=500),
+            locations=loc.column_header(),
+        )
+    )
